9ab16968a967c5d2aaaddabc961da97032c71de3
arichardi <a.richardi@gmail.com>
2021-10-26 11:53:09 -0300

    styles colors and fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+}  from '@expo-google-fonts/inter'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+}  from '@expo-google-fonts/archivo'
+
 import HomeScreen from './app/screens/HomeScreen';
+import theme from './app/Styles/theme';
 
 
 export default function App() {
+
+  //fonts load
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <HomeScreen />
+    <ThemeProvider theme={theme}>
+      <HomeScreen />
+    </ThemeProvider>
     );
 }
