3d038f238aa28aff57eaac64fc463f755643b976
arichardi <a.richardi@gmail.com>
2021-10-26 12:28:55 -0300

    header component

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
 import {
     Container,
-    Title,
+    Header,
+    TotalCars,
+    HeaderContent,
 } from './HomeScreenStyle'
+
+import Logo from '../assets/logo.svg'
 
 export default function HomeScreen(){
     return (
         <Container>
-            <Title>Home</Title>
+            <StatusBar 
+                barStyle='light-content'
+                translucent
+                backgroundColor='transparent'
+            />
+            <Header>  
+                <HeaderContent> 
+                <Logo 
+                    width={ RFValue(108)}
+                    height={ RFValue(12)}
+                />
+                <TotalCars>Total de 12 carros</TotalCars>
+                </HeaderContent>
+            </Header>
         </Container>
         )
 }
--- app/screens/HomeScreenStyle.ts
+++ app/screens/HomeScreenStyle.ts
@@ -1,14 +1,30 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
 
     background-color: ${ ({theme}) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-    font-size: 30px;
-    font-family: ${ ({theme}) => theme.fonts.secondary600};
+export const Header = styled.View`
+    width: 100%;
+    height: 113px;
+    background-color: ${ ({theme}) => theme.colors.header};
+    justify-content: flex-end;
+    padding: 32px 24px;
+    `;
+
+export const TotalCars = styled.Text`
+    font-size: ${ RFValue(14)}px;
+    font-family: ${({theme}) => theme.fonts.primary400};
+    color: ${({theme}) => theme.colors.text};
+    
+    `;
+
+export const HeaderContent = styled.View`
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+
 `;
