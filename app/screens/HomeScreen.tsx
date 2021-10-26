fb64d00c74362a72200f9438a9f49bb3ce3ba160
arichardi <a.richardi@gmail.com>
2021-10-26 11:04:04 -0300

    styled component + home

---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+    Container,
+    Title,
+} from './HomeScreenStyle'
+
+export default function HomeScreen(){
+    return (
+        <Container>
+            <Title>Home</Title>
+        </Container>
+        )
+}
