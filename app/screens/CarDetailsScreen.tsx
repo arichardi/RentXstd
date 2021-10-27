83b1471c9653fe46ca1e0fd7ab2d39eebe532bee
arichardi <a.richardi@gmail.com>
2021-10-27 11:40:49 -0300

    cardetails WIP 2

--- app/screens/CarDetailsScreen.tsx
+++ app/screens/CarDetailsScreen.tsx
@@ -3,6 +3,15 @@ import {
 Container,
 Header,
 CarImage,
+Content,
+Details,
+Description,
+Brand,
+Name,
+Rent,
+Period,
+Price,
+About,
 } from './CarDetailsScreenStyle'
 import BackButton from '../Components/BackButton';
 import ImageSlider from '../Components/ImageSlider';
@@ -19,6 +28,23 @@ return (
         imagesUrl={['https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png',]}
     />
     </CarImage>
+
+    <Content>
+        <Details>
+        <Description>
+            <Brand>Audi</Brand>
+            <Name>5S Coupe</Name>
+        </Description>
+
+        <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 500</Price>
+        </Rent>
+        </Details>
+
+        <About>Bla bla bla bla</About>
+
+    </Content>
 
 </Container>
 );
